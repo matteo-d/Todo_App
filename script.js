d7937d12e89170c89b77319a9775abd89122a0a1
matteo-d <matteodrouin33@gmail.com>
2021-03-09 14:04:02 +0100

    bloquer a local storage, 4h34

--- script.js
+++ script.js
@@ -1,69 +1,35 @@
 const form = document.getElementById("form");
 const input = document.getElementById("input");
-const todosUL = document.getElementById("todos");
-
-const todos = JSON.parse(localStorage.getItem("todos"));
-
-if (todos) {
-    todos.forEach((todo) => {
-        addTodo(todo);
-    });
-}
-
-form.addEventListener("submit", (e) => {
+// todos = Ul
+const todos = document.getElementById("todos");
+// Lorsque le le form est soumit...
+form.addEventListener('submit', (e) => {
+    //Empeche le reload de la page ( comportement nromal submition sur le form)
     e.preventDefault();
+    // input = valeur taper dans l'input
+    const todoText = input.value;
+    // Si l'input a une value entré
+    if(todoText) {
+        // Créer un li sous le ul
+        const todoEl = document.createElement('li');
+        // Le texte du li sera l'input de l'user
+        todoEl.innerText = todoText;
+        // Interrupteur activation class / desactivation
+        todoEl.addEventListener('click', () => {
+            todoEl.classList.toggle('completed');
+        });
+        // Retirer l'élément todo au clic droit grace a contextmenu
+        todoEl.addEventListener('contextmenu',  (e) => {
+            e.preventDefault();
+            todoEl.remove();
+        })
+        // Pas compris
+        todos.appendChild(todoEl);
 
-    addTodo();
+        // Vider l'input après submition
+        input.value = ''; 
+    }
 });
 
-function addTodo(todo) {
-    let todoText = input.value;
+// Stocker les notes en local
 
-    if (todo) {
-        todoText = todo.text;
-    }
-
-    if (todoText) {
-        const todoEl = document.createElement("li");
-        if (todo && todo.completed) {
-            todoEl.classList.add("completed");
-        }
-
-        todoEl.innerText = todoText;
-
-        todoEl.addEventListener("click", () => {
-            todoEl.classList.toggle("completed");
-
-            updateLS();
-        });
-
-        todoEl.addEventListener("contextmenu", (e) => {
-            e.preventDefault();
-
-            todoEl.remove();
-
-            updateLS();
-        });
-
-        todosUL.appendChild(todoEl);
-
-        input.value = "";
-
-        updateLS();
-    }
-}
-
-function updateLS() {
-    const todosEl = document.querySelectorAll("li");
-
-    const todos = [];
-
-    todosEl.forEach((todoEl) => {
-        todos.push({
-            text: todoEl.innerText,
-            completed: todoEl.classList.contains("completed"),
-        });
-    });
-
-    localStorage.setItem("todos", JSON.stringify(todos));
-}
